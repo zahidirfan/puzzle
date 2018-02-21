b3a188279c8dd2ebffd02e074813a82f41f4e433
Zahid Irfan <zahid.irfan@gmail.com>
2018-02-21 21:47:56 +0500

    Adding License and someother components

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -10,10 +10,10 @@ export default class Canvas extends Component {
   render (){
 
     return (
-      <ReactGridLayout className="layout" rows={1} cols={3} rowHeight={30} width={300} autoSize={false}>
-        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 1,}}>a</div>
-        <div key="b" data-grid={{x: 1, y: 0, w: 1, h: 1,}}>b</div>
-        <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 1}}>c</div>
+      <ReactGridLayout className="layout" maxrows={1} isResizable={false} isDraggable={true} cols={3} height= {1000} rowHeight={30} width={300} static={true} autoSize={false}>
+        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 1, minW : 3, maxW : 1}}>a</div>
+        <div key="b" data-grid={{x: 1, y: 0, w: 1, h: 1, minW : 3, maxW : 1}}>b</div>
+        <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 1, minW : 3, maxW : 1}}>c</div>
       </ReactGridLayout>
     );
 
